90b7d22fb2b5947e7428bb1d93d716a6565f1aad
hedie <rahimihedie22@gmail.com>
2023-07-02 14:10:41 +0330

    worked on some warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This App is
+        <a
+          href="https://github.com/hedoshro/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        created by Hedie Rahimi
+      </footer>
+    </div>
+  );
 }
 
 export default App;
